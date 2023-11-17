31590ae184bd8a8c6978f97d5712ea99a1c91ee1
SAHIL PRASAD <sahilprasad033@gmail.com>
2023-11-18 04:01:16 +0530

    server sidebar

---
+++ components/server/server-header.tsx
@@ -0,0 +1,91 @@
+'use client';
+import React from 'react';
+import {ServerWithMembersWithProfiles} from "@/types";
+import {MemberRole} from "@prisma/client";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users} from "lucide-react";
+
+
+interface ServerHeaderProps {
+	server: ServerWithMembersWithProfiles;
+	role?: MemberRole;
+}
+
+const ServerHeader = ({server, role}: ServerHeaderProps) => {
+
+	const isAdmin = role === MemberRole.ADMIN;
+	const isModerator = role === MemberRole.MODERATOR;
+
+
+	return (
+		<div>
+			<DropdownMenu>
+				<DropdownMenuTrigger className='focus:outline-none' asChild>
+					<button
+						className='w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-300 dark:border-neutral-800 border-b-2 hover:bg-gray-700/10 dark:hover:bg-zinc-700/50 transition'
+					>
+						{server.name}
+						<ChevronDown className='ml-auto h-5 w-5'/>
+					</button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-gray-300 space-y-[2px]'>
+					{(isModerator||isAdmin) && (
+						<DropdownMenuItem
+							className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'>
+							Invite People
+							<UserPlus className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+					{isAdmin && (
+						<DropdownMenuItem
+							className=' px-3 py-2 text-sm cursor-pointer'>
+							Server Settings
+							<Settings className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+					{isAdmin && (
+						<DropdownMenuItem
+							className=' px-3 py-2 text-sm cursor-pointer'>
+							Manage Members
+							<Users className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+					{(isModerator||isAdmin) && (
+						<DropdownMenuItem
+							className=' px-3 py-2 text-sm cursor-pointer'>
+							Create channel
+							<PlusCircle className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+					{(isModerator||isAdmin) && (
+						<DropdownMenuSeparator/>
+					)}
+					{isAdmin && (
+						<DropdownMenuItem
+							className='text-red-600 dark:text-red-400 px-3 py-2 text-sm cursor-pointer'>
+							Delete Server
+							<Trash className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+					{!isAdmin && (
+						<DropdownMenuItem
+							className='text-red-600 dark:text-red-400 px-3 py-2 text-sm cursor-pointer'>
+							Leave Server
+							<LogOut className='ml-auto h-4 w-4'/>
+						</DropdownMenuItem>
+					)}
+				</DropdownMenuContent>
+
+			</DropdownMenu>
+
+		</div>
+	);
+};
+
+export default ServerHeader;
---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {currentProfile} from "@/lib/current-profile";
+import {redirect} from "next/navigation";
+import {db} from "@/lib/db";
+import {ChannelType} from "@prisma/client";
+import ServerHeader from "@/components/server/server-header";
+
+interface ServerSidebarProps {
+	serverId: string
+}
+
+const ServerSidebar = async ({serverId}: ServerSidebarProps) => {
+
+	const profile = await currentProfile();
+	if (!profile) {
+		return redirect('/')
+	}
+	const server = await db.server.findUnique({
+		where: {
+			id: serverId
+		},
+		include: {
+			channels: {
+				orderBy: {
+					createdAt: 'asc'
+
+				}
+			},
+			members: {
+				include: {
+					profile: true,
+				},
+				orderBy: {
+					role: 'asc'
+				}
+			}
+		}
+	});
+
+	const textChannels = server?.channels.filter(channel => channel.type === ChannelType.TEXT);
+	const audioChannels = server?.channels.filter(channel => channel.type === ChannelType.AUDIO);
+	const videoChannels = server?.channels.filter(channel => channel.type === ChannelType.VIDEO);
+	const members = server?.members.filter(member => member.profileId !== profile.id);
+
+	if(!server) {
+		redirect('/');
+	}
+	const role = server?.members.find(member => member.profileId === profile.id)?.role;
+
+	return (
+		<div className='flex flex-col h-full text-primary w-full dark:bg-[#393934] bg-[#e9e0d7]'>
+			<ServerHeader
+			server={server}
+			role={role}
+			/>
+		</div>
+	);
+};
+
+export default ServerSidebar;
