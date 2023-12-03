c38c519b68398ba95bfb40ad890f28f57bdb1f06
SAHIL PRASAD <sahilprasad033@gmail.com>
2023-12-03 14:33:42 +0530

    member manage modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -54,6 +54,7 @@ const ServerHeader = ({server, role}: ServerHeaderProps) => {
 					)}
 					{isAdmin && (
 						<DropdownMenuItem
+							onClick={() => onOpen('members', {server})}
 							className=' px-3 py-2 text-sm cursor-pointer'>
 							Manage Members
 							<Users className='ml-auto h-4 w-4'/>
