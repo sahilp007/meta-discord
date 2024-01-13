85066fe2b6959a3baf02182dabf2469ee451a1b6
SAHIL PRASAD <sahilprasad033@gmail.com>
2024-01-13 09:56:41 +0530

    Conversations Header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Hash} from "lucide-react";
+import {Hash, User} from "lucide-react";
 import {MobileToggle} from "@/components/mobile-toggle";
+import Image from "next/image";
 
 
 interface ChatHeaderProps {
@@ -12,14 +13,19 @@ interface ChatHeaderProps {
 
 export const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) => {
 	debugger;
-	return (
-		<div className='text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2'>
-			<MobileToggle serverId={serverId}/>
-			{type === 'channel' && (
-				<Hash className='text-neutral-500 dark:text-neutral-400 h-6 w-6 mr-2'/>
-			)}
-			<p className='font-semibold text-md text-black dark:text-zinc-100'>
-				{name}
-			</p>
-		</div>);
+	return (<div
+		className='text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2'>
+		<MobileToggle serverId={serverId}/>
+		{type === 'channel' && (<Hash className='text-neutral-500 dark:text-neutral-400 h-6 w-6 mr-2'/>)}
+		{type === 'conversation' && (imageUrl !== undefined ?
+			(<Image alt='avatar'
+			        src={imageUrl}
+			        width={36}
+			        height={36}
+			        className='rounded-full select-none none mr-2'/>) : (
+				<User/>))}
+		<p className='font-semibold text-md text-black dark:text-zinc-100'>
+			{name}
+		</p>
+	</div>);
 }
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -64,7 +64,7 @@ const ServerSidebar = async ({serverId}: ServerSidebarProps) => {
 	}
 	const role = server?.members.find(member => member.profileId === profile.id)?.role;
 
-	return (<div className='flex flex-col h-full text-primary w-full dark:bg-[#393934] bg-[#e9e0d7]'>
+	return (<div className='flex flex-col h-full text-primary w-full dark:bg-[#2e2e2a] bg-[#e9e0d7]'>
 			<ServerHeader
 				server={server}
 				role={role}
